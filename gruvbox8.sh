806a98d58736e1b9ecadaa8a6ef61c3ff38e576c
mauricesvp <maurice@walny.de>
2023-04-03 20:08:14 +0200

    vundle and gruvbox scripts

---
+++ gruvbox8.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir -p ~/.vim/colors
+wget https://raw.githubusercontent.com/lifepillar/vim-gruvbox8/master/colors/gruvbox8.vim -P ~/.vim/colors/
---
+++ vundle.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir -p ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
